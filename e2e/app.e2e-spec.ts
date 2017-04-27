bcf99d873726110892242407acf7e4c6a285be76
lthompson <lthompson@infomedia.com.au>
2017-04-28 09:27:02 +1000

    ng new ng-superpowers --directory . --routing true

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { NgSuperpowersPage } from './app.po';
+
+describe('ng-superpowers App', () => {
+  let page: NgSuperpowersPage;
+
+  beforeEach(() => {
+    page = new NgSuperpowersPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
