d091a92755075695bfb6252a8235cdef7f0e700c
lthompson <lthompson@infomedia.com.au>
2017-04-28 09:56:43 +1000

    First lesson!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>{{title}}</h1>
+             Current time: {{currentDate}}
+             <button (click)='refreshTime()'>Refresh</button>
+             <br/>
+             <label for='titleChange'>Change title:</label>
+             <input id='titleChange' [(ngModel)]='title'/>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular SuperPowers';
+  currentDate: Date = new Date();
+
+  refreshTime() {
+    this.currentDate = new Date();
+  }
 }
