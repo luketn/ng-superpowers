295a5d3df483b05bb2825e50c7272ef4d60f34a5
lthompson <lthompson@infomedia.com.au>
2017-04-28 10:52:02 +1000

    Add the company list component with: git commit --amend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyListComponent } from './company/company-list/company-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyListComponent
   ],
   imports: [
     BrowserModule,
