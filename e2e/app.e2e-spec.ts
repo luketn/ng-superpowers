7588f8673683441f94a9fc15f480b425bf44db5d
lthompson <lthompson@infomedia.com.au>
2017-04-28 10:18:16 +1000

    Start again with: ng new ng-superpowers --d irectory=. --routing=true --style=scss --prefix=ngsp (adding a prefix)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('ng-superpowers App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('ngsp works!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `<h1>{{title}}</h1>
-             Current time: {{currentDate}}
-             <button (click)='refreshTime()'>Refresh</button>
-             <br/>
-             <label for='titleChange'>Change title:</label>
-             <input id='titleChange' [(ngModel)]='title'/>
-  `,
+  selector: 'ngsp-root',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular SuperPowers';
-  currentDate: Date = new Date();
-
-  refreshTime() {
-    this.currentDate = new Date();
-  }
+  title = 'ngsp works!';
 }
